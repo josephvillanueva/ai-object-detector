4d5c97d2ca648730f46e7cf45d9fd99d1c4feb39
Joseph Villanueva <josephgvillanueva24@gmail.com>
2024-04-24 04:22:24 +0800

    fix error on function

--- component/object-detection.js
+++ component/object-detection.js
@@ -1,10 +1,11 @@
-return (
-  <div className="mt-8">
-    <div className="relative flex justify-center items-center gradient p-1.5 rounded-md">
-      {/* webcam */}
-      {/* canvas */}
+const ObjectDetection = () => {
+  return (
+    <div className="mt-8">
+      <div className="relative flex justify-center items-center gradient p-1.5 rounded-md">
+        {/* webcam */}
+        {/* canvas */}
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default ObjectDetection;
