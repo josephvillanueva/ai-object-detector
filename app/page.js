12c14ec0cd989625d94a4e6a0a59e0e7834b7e38
Joseph Villanueva <josephgvillanueva24@gmail.com>
2024-04-24 04:21:53 +0800

    Create ObjectDetection component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+import ObjectDetection from "@/component/object-detection";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
       <h1 className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">
         AI Object Detector
       </h1>
+      <ObjectDetection />
     </main>
   );
 }
