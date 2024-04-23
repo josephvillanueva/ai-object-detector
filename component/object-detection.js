62e58559446686c5920dbf4297fb11390d367901
Joseph Villanueva <josephgvillanueva24@gmail.com>
2024-04-24 04:25:29 +0800

    call Webcam

--- component/object-detection.js
+++ component/object-detection.js
@@ -1,8 +1,14 @@
+"use client";
+
+import React from "react";
+import Webcam from "react-webcam";
+
 const ObjectDetection = () => {
   return (
     <div className="mt-8">
       <div className="relative flex justify-center items-center gradient p-1.5 rounded-md">
         {/* webcam */}
+        <Webcam className="rounded-md w-full lg:h-[720px]" muted />
         {/* canvas */}
       </div>
     </div>
